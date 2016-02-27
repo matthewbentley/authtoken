43721b0554549feb3685781b7a260fec6b154a3a
Matthew Bentley <matthew.t.bentley@gmail.com>
2016-02-26 21:53:54 -0500

    Add re-export

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,6 @@ extern crate hyper;
 extern crate cookie;
 
 pub mod authtoken;
+
+pub use authtoken::{AuthToken, TokenError, set_auth_cookie, verify_auth_cookie,
+                    get_auth_token_from_headers};
